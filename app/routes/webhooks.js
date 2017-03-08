551ab0b88ffe3d48fcd9e58d24d5dd676adee5a0
Martin Fedec <martin@amplitudesolutions.ca>
2017-03-07 19:41:47 -0800

    Added promises to handle adding items so that it doesn't execute async

--- app/routes/webhooks.js
+++ app/routes/webhooks.js
@@ -1,5 +1,5 @@
+var Q = require('q');
 // Firebase
-
 var firebase = require("firebase-admin");
 // require("../../config/mealtimeprod-firebase.json") || 
 
@@ -14,63 +14,90 @@ firebase.initializeApp({
   	databaseURL: process.env.mealtimeDatabaseURL
 });
 
+function addNewItem(elements) {
+	var deferred = Q.defer();
+
+    var data = firebase.database().ref('/ff84cdcc-7349-4fbb-b11b-795588e03226');
+	var itemsList = data.child('items');
+
+	var item = {
+		name: '',
+		searchValue:''
+	};
+
+	// elements.forEach(function(element) {
+
+	console.log(elements[0]);
+	item.name = elements[0];
+	item.searchValue = elements[0].toLowerCase();
+
+	itemsList.orderByChild("searchValue").startAt(item.searchValue).endAt(item.searchValue).once('value', function(dataSnapshot) {
+		if (dataSnapshot.val() === null) {
+			console.log('Adding ' + item.name);
+			item.category = '-JxxMBHu93IyJOu365hA';
+			item = itemsList.push(item);
+			var list = data.child('lists/Default/items/' + item.key);
+
+			// var categoryItem = data.child('categories/' + '-JxxMBHu93IyJOu365hA/' + 'items');
+			// categoryItem.set({})
+    		
+    		list.set({category: item.val().category, gotit: false, quantity: 1}, function(subRes) {
+    			deferred.resolve(item.val().name);
+    		});
+
+			// CREATE NEW ITEM
+          // //Create New Item
+          // items.$add(item).then(function(ref) {         
+          //     categoriesRef.child("/" + defaultCategory + "/items/" + ref.key()).set(true);
+          //     deferred.resolve(items.$getRecord(ref.key()));
+          //   });
+        } else {
+        	dataSnapshot.forEach(function(res) {
+        		var list = data.child('lists/Default/items/' + res.key);
+        		list.set({category: res.val().category, gotit: false, quantity: 1}, function(subRes) {
+        			deferred.resolve(res.val().name);	
+        		});
+        	});
+        }
+    });
+    // });
+
+    return deferred.promise;
+};
+
 module.exports = function(router) {
     
     router.route('/webhook')
         .post(function(req, res) { 
         	var addList = req.body.result.parameters['Add-list'];
 
-        	
+			// var categoryList = data.child('categories');
 
-			var data = firebase.database().ref('/ff84cdcc-7349-4fbb-b11b-795588e03226');
+			
 
-			var itemsList = data.child('items');
+		    // categoryList.orderByChild('default').startAt(true).endAt(true).once('value', function(snap) {
+		    // 	console.log(snap.val());
+		    //     // snap.forEach(function(snapData) {
+		    //       item.category = snap.key;
+		    //     // });
 
-			var item = {
-				name: '',
-				searchValue:''
-			}
+		        // addList.forEach(function(element) {
+					addNewItem(addList).then(function(data) {
+						
 
-			// for (var i = 0; i < addList.length; i++) {
-			addList.forEach(function(element) {
-				console.log(element);
-				item.name = element;
-				item.searchValue = element.toLowerCase();
+						res.json({
+						  	"speech": "Alright, " + data + " have been added",
+						  	"source": "mealtime-api",
+							"displayText": "Alright, " + data + " have been added"
+			        	});
+					});
+				// });
+		    // });
 
-				return itemsList.orderByChild("searchValue").startAt(item.searchValue).endAt(item.searchValue).once('value', function(dataSnapshot) {
-					if (dataSnapshot.val() === null) {
-						console.log('Adding ' + item.name);
-						return itemsList.push().set(item);
-						// CREATE NEW ITEM
-		              // //Create New Item
-		              // items.$add(item).then(function(ref) {         
-		              //     categoriesRef.child("/" + defaultCategory + "/items/" + ref.key()).set(true);
-		              //     deferred.resolve(items.$getRecord(ref.key()));
-		              //   });
-		            } else {
-		              	console.log('Add item to list.');
-
-		              	// ADD ITEM TO LIST
-
-		                // dataSnapshot.forEach(function(snap){
-		                //   deferred.resolve(items.$getRecord(snap.key()));
-		                // });
-		            }
-		        });
-
-			});
+			
 			// data.once('value').then(function(snap) {
 			// 	console.log(snap.val());
 			// });
-
-
-        	// console.log(req.body.result.parameters);
-        	res.json({
-			  	"speech": "Sure, I have added " + item.name,
-			  	"source": "mealtime-api",
-				"displayText": "Sure, I have added " + item.name
-        	});
-
         })
     ;
 
